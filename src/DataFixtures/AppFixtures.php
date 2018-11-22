ed0dc2cc6ecf97519547dfa0af1b0e59f3c5c714
Estérfano Lopes <esterfano.lopes@gmail.com>
2018-11-22 15:50:23 -0200

    Lecture 29. Creating fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\MicroPost;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $microPost = new MicroPost();
+
+            $microPost->setText('Some random text '.rand(0, 100));
+            $microPost->setTime(new \DateTime(rand(1999, 2999).'-'.rand(1, 12).'-'.rand(1, 27)));
+            $manager->persist($microPost);
+        }
+
+        $manager->flush();
+    }
+}
