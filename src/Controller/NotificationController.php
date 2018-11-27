3b40455441294e57e581c33f213dab259de8db41
Estérfano Lopes <esterfano.lopes@gmail.com>
2018-11-27 11:32:22 -0200

    Lecture 70. NotificationRepository - fetching notifications unseen by user

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: esterfano
+ * Date: 27/11/18
+ * Time: 11:27
+ */
+
+namespace App\Controller;
+
+use App\Repository\NotificationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Security(is_granted('ROLE_USER')"
+ * @Route("/notification")
+ */
+class NotificationController extends AbstractController
+{
+    /** @var NotificationRepository */
+    private $notificationRepository;
+
+    public function __construct(NotificationRepository $notificationRepository)
+    {
+        $this->notificationRepository = $notificationRepository;
+    }
+
+    /**
+     * @Route("/unread-count", name="notification_unread")
+     */
+    public function unreadCount()
+    {
+        new JsonResponse([
+            'count' => $this->notificationRepository->findUnseenByUser($this->getUser())
+        ]);
+    }
+}
