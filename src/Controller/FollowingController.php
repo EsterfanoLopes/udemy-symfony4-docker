3e4c02e7345846a599e2f537da388d535221ee80
Estérfano Lopes <esterfano.lopes@gmail.com>
2018-11-27 08:56:43 -0200

    Lecture 58. Follow / Unfollow button (controller, adding Entities to collection)

---
+++ src/Controller/FollowingController.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: esterfano
+ * Date: 26/11/18
+ * Time: 17:00
+ */
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Security("is_granted('ROLE_USER')")
+ * @Route()
+ */
+class FollowingController extends AbstractController
+{
+    /**
+     * @Route("/follow/{id}", name="following_follow")
+     */
+    public function follow(User $userToFollow)
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        $currentUser->getFollowing()
+            ->add($userToFollow);
+
+        $this->getDoctrine()
+            ->getManager()
+            ->flush();
+
+        return $this->redirectToRoute(
+            'micro_post_user',
+            ['username' => $userToFollow->getUsername()]
+        );
+    }
+
+    /**
+     * @Route("/unfollow/{id}", name="following_unfollow")
+     */
+    public function unfollow(User $userToUnfollow)
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        $currentUser->getFollowing()
+            ->removeElement($userToUnfollow);
+
+        $this->getDoctrine()
+            ->getManager()
+            ->flush();
+
+        return $this->redirectToRoute(
+            'micro_post_user',
+            ['username' => $userToUnfollow->getUsername()]
+        );
+    }
+}
