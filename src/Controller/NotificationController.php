1092d9b7c08e2740e7ebbd892a7629801f114ba4
Estérfano Lopes <esterfano.lopes@gmail.com>
2018-11-27 14:37:26 -0200

    Lecture 74. Marking notifications as being read (Doctrine Query Builder UPDATE)

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Notification;
 use App\Repository\NotificationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -48,5 +49,27 @@ class NotificationController extends AbstractController
                 'user' => $this->getUser()
             ])
         ]);
+    }
+
+    /**
+     * @Route("/acknowledge/{id}", name="notification_acknowledge")
+     */
+    public function acknowledge(Notification $notification)
+    {
+        $notification->setSeen(true);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute('notification_all');
+    }
+
+    /**
+     * @Route("/acknowledge-all", name="notification_acknowledge_all")
+     */
+    public function acknowledgeAll()
+    {
+        $this->notificationRepository->markAllAsReadByUser($this->getUser());
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute('notification_all');
     }
 }
