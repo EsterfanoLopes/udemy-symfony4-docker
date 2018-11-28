d69a1ae4df486983ac1a54355f9f18efc048db81
Estérfano Lopes <esterfano.lopes@gmail.com>
2018-11-28 15:20:53 -0200

    Lecture 96. A more complicated case with testing

--- tests/Mailer/MailerClassTest.php
+++ tests/Mailer/MailerClassTest.php
@@ -19,15 +19,35 @@ class MailerClassTest extends TestCase
         $user = new User();
         $user->setEmail('john@mail.com');
 
-        $swiftMailer = $this->getMockBuilder(\Swift_Mailer::class)
+        # SwiftMailer mock tests
+        $swiftMailerMock = $this->getMockBuilder(\Swift_Mailer::class)
             ->disableOriginalConstructor()
             ->getMock();
+        $swiftMailerMock->expects($this->once())
+            ->method('send')
+            ->with($this->callback(function($subject) {
+                $messageString = (string)$subject;
 
+                return strpos($messageString, "From: me@from.me") !== false
+                    && strpos($messageString, "Content-Type: text/html; charset=utf-8") !== false
+                    && strpos($messageString, "Subject: Welcome to the micro-post app!") !== false
+                    && strpos($messageString, "To: john@mail.com") !== false
+                    && strpos($messageString, "This is a message body") !== false;
+            }));
+
+        # Twig mock tests
         $twigMock = $this->getMockBuilder(\Twig_Environment::class)
             ->disableOriginalConstructor()
             ->getMock();
+        $twigMock->expects($this->once())
+            ->method('render')
+            ->with('email/registration.html.twig',
+                [
+                    'user' => $user,
+                ]
+            )->willReturn('This is a message body');
 
-        $mailer = new MailerClass($swiftMailer, $twigMock, $user->getEmail());
+        $mailer = new MailerClass($swiftMailerMock, $twigMock, "me@from.me");
         $mailer->sendConfirmationEmail($user);
     }
 }
