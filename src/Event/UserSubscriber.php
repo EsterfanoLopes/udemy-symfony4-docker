435ea840ebad8d72a25420c6dbfd06bf07a37e0a
Estérfano Lopes <esterfano.lopes@gmail.com>
2018-11-28 11:53:02 -0200

    Lecture 91. Creating new UserPreferences after user registration (responding to event)

--- src/Event/UserSubscriber.php
+++ src/Event/UserSubscriber.php
@@ -8,19 +8,26 @@
 
 namespace App\Event;
 
+use App\Entity\UserPreferences;
 use App\Mailer\MailerClass;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UserSubscriber implements EventSubscriberInterface
 {
-    /**
-     * @var Mailer
-     */
+    /*** @var MailerClass */
     private $mailer;
 
-    public function __construct(MailerClass $mailer)
+    /** @var EntityManagerInterface */
+    private $entityManager;
+    /*** @var string */
+    private $defaultLocale;
+
+    public function __construct(MailerClass $mailer, EntityManagerInterface $entityManager, string $defaultLocale)
     {
         $this->mailer = $mailer;
+        $this->entityManager = $entityManager;
+        $this->defaultLocale = $defaultLocale;
     }
 
     public static function getSubscribedEvents()
@@ -32,6 +39,14 @@ class UserSubscriber implements EventSubscriberInterface
 
     public function onUserRegister(UserRegisterEvent $event)
     {
+        $preferences = new UserPreferences();
+        $preferences->setLocale($this->defaultLocale);
+
+        $user = $event->getRegisteredUser();
+        $user->setPreferences($preferences);
+
+        $this->entityManager->flush();
+
         $this->mailer->sendConfirmationEmail($event->getRegisteredUser());
     }
 }
